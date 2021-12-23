8d8ed4ce2b5979f7818cd575da59b524b9a9cb46
ahamada <ahamada>
2021-12-23 16:33:23 +0100

    Verzia cislo 2 pridal som dve textove polia na vlozenie dat a tiez dve tlacidla na zobrazenie dat, data sa uz pridavaju do Sqlite databazy, ale v aplikacii sa zatial este nezobrazuju

--- app/src/main/java/com/example/anglictinaapp/MainActivity.kt
+++ app/src/main/java/com/example/anglictinaapp/MainActivity.kt
@@ -2,10 +2,65 @@ package com.example.anglictinaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var edName: EditText
+    private lateinit var edEmail: EditText
+    private lateinit var btnAdd: Button
+    private lateinit var btnView: Button
+
+    private lateinit var sqliteHelper: SQLiteHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+        sqliteHelper = SQLiteHelper(this)
+
+        btnAdd.setOnClickListener { addStudent() }
+        btnView.setOnClickListener { getStudents() }
     }
+
+    private fun getStudents() {
+        val stdList = sqliteHelper.getAllStudents()
+        Log.e("pppp", "${stdList.size}")
+    }
+
+    private fun addStudent() {
+        val name = edName.text.toString()
+        val email = edEmail.text.toString()
+
+        if (name.isEmpty()  || email.isEmpty()) {
+            Toast.makeText(this, "Please eneter required field ", Toast.LENGTH_SHORT).show()
+        }else {
+            val std = StudentModel(name = name, email = email)
+            val status = sqliteHelper.insertStudent(std)
+            //Kontrola vlozenia zaznamu
+            if (status > -1) {
+                Toast.makeText(this, "Student Added...", Toast.LENGTH_SHORT).show()
+                clearEditText()
+            } else {
+                Toast.makeText(this, "Record not saved", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun clearEditText() {
+        edName.setText("")
+        edEmail.setText("")
+        edName.requestFocus()
+    }
+
+    private fun initView() {
+        edName = findViewById(R.id.edname)
+        edEmail = findViewById(R.id.edEmail)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnView = findViewById(R.id.btnView)
+    }
+
 }
